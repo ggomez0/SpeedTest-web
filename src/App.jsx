b38ed199f7e41c1d47516e1b13bea7f104f0e89c
ggomez0 <gaspargomez2000@outlook.com>
2025-01-31 21:29:59 -0300

    feat: update navigation links and add footer to App component

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,22 @@ function App() {
           <div className='mr-100'></div>
         </div>
         <div className="hidden md:flex space-x-10 font-bold-500">
-          <a href="#" className="text-gray-300 hover:text-blue-300">Apps</a>
-          <a href="#" className="text-gray-300 hover:text-blue-300">Learn</a>
-          <a href="#" className="text-gray-300 hover:text-blue-300">Data</a>
-          <a href="#" className="text-gray-300 hover:text-blue-300">About</a>
+          <a href="https://www.speedtest.net/apps" className="text-gray-300 hover:text-blue-300">Apps</a>
+          <a href="https://www.speedtest.net/about/knowledge/glossary" className="text-gray-300 hover:text-blue-300">Learn</a>
+          <a href="https://www.ookla.com/articles" className="text-gray-300 hover:text-blue-300">Data</a>
+          <a href="https://www.speedtest.net/apps" className="text-gray-300 hover:text-blue-300">About</a>
         </div>
         <div className="flex space-x-2">
-          <img src={help} alt="vite logo" className="h-6 w-4" />
+          <a href='https://www.speedtest.net/help'><img src={help} alt="vite logo" className="h-6 w-4" /></a>
           <img src={account} alt="react logo" className="h-6 w-4" />
         </div>
       </nav>
 
       <Speedtest />
+
+      <footer className="fixed bottom-0 w-full py-4 flex justify-center items-center">
+        <p className="text-gray-300 text-sm">© 2025 - Speedtest like Ookla</p>
+      </footer>
 
     </>
   )
